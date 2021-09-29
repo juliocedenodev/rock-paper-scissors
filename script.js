760abbf40427a6fa53cd06bb5639a20e8768b39b
Julio <juliocedenodev@gmail.com>
2021-09-28 21:05:56 -0400

    getScore function created

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let cpuWin = `You Lose this round`;
 let playerWin =  `You Win this round`; 
 let draw = `It's a Tie!, try again`;
+
 let playerWins = "You Win, Congratulations!";
 let cpuWins = " You Lose, Too bad!";
+
 let playerScore = 0;
 let cpuScore = 0;
 let drawScore = 0;
@@ -10,63 +12,78 @@ let drawScore = 0;
 function cpu(selection)
 {
     let options = ["rock", "paper", "scissors"]
-    selection= options[Math.floor(Math.random() * options.length)]
+    selection = options[Math.floor(Math.random() * options.length)]
     return selection;
 }
 
 function round(playerSelection, cpuSelection)
 {
-  if (cpuSelection === "rock" && playerSelection === "scissors" || cpuSelection === "paper" && playerSelection === "rock" || cpuSelection === "scissors" && playerSelection=== "paper" ) {
+  if (cpuSelection === "rock" && playerSelection === "scissors" || 
+      cpuSelection === "paper" && playerSelection === "rock" || 
+      cpuSelection === "scissors" && playerSelection=== "paper") 
+    {
       return cpuWin;
-  }
-  else if(playerSelection=== "rock" && cpuSelection === "scissors" || playerSelection=== "paper" && cpuSelection === "rock" || playerSelection=== "scissors" && cpuSelection === "paper")
-  {
-    return playerWin; 
-  }
+    }
+
+  else if (playerSelection=== "rock" && cpuSelection === "scissors" || 
+           playerSelection=== "paper" && cpuSelection === "rock" || 
+           playerSelection=== "scissors" && cpuSelection === "paper")
+    {
+      return playerWin; 
+    }
+    
   else if(playerSelection === cpuSelection)
-  {
-     return draw;
-  }
+    {
+      return draw;
+    }
 }
 
 
   for (let i = 0; i< 1000; i++) {
  
-  let playerSelection = prompt("Make your choice:").toLowerCase();
-  let cpuSelection = cpu();
-  result = round(playerSelection, cpuSelection);
-  console.log(result);
+    let playerSelection = prompt("Make your choice:").toLowerCase();
+    let cpuSelection = cpu();
+    let result = round(playerSelection, cpuSelection);
 
-  if(result == playerWin)
-  {
-    playerScore++;
-    
-  }
-  else if(result == cpuWin)
-  {
-    cpuScore++;
-    
-  }
-  else if(result == draw)
-  {
-    drawScore++;
-  }
+    console.log(result);
+    getScore(result);
 
-  console.log(`Player Score: ${playerScore}`);
-  console.log(`CPU Score: ${cpuScore}`);
-  console.log(`Draws: ${drawScore}`);
-
-  if(playerScore === 5)
-  {
-    console.log(playerWins);
-    break;
-  }
-  else if(cpuScore  === 5)
-  {
-    console.log(cpuWins);
-    break;
-  }
-
+    if (playerScore === 5 || cpuScore === 5) {
+      break;
+    }
 }
 
 
+
+
+
+function getScore(result)
+{
+  if (result == playerWin)
+    {
+      playerScore++;
+    }
+    else if (result == cpuWin)
+    {
+      cpuScore++;
+    }
+    else if (result == draw)
+    {
+      drawScore++;
+    }
+
+    console.log(`Player Score: ${playerScore}`);
+    console.log(`CPU Score: ${cpuScore}`);
+    console.log(`Draws: ${drawScore}`);
+
+    if (playerScore === 5)
+    {
+      console.log(playerWins);
+      return;
+    }
+    else if (cpuScore === 5)
+    {
+      console.log(cpuWins);
+      return;
+    }
+}
